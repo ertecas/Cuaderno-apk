7b49e78dae28d6179261e36456766fdc7b789e34
ertecas <100793611+ertecas@users.noreply.github.com>
2022-03-27 13:48:59 +0200

    Update Profesor.c

--- Profesor.c
+++ Profesor.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+#include "carga.h"
+#include "datos.h"
+#include "perfil.h"
+
+void perfil (tipo,i_usuario) { //Tipo recibe 1 o 0 | i_usuario es el indice del usuario iniciado
+	
+	if (tipo==1) {
+		
+		menu_admin (i_usuario);
+		
+	} else {
+		
+		menu_profe (i_usuario);
+		
+	}
+}
+
+void menu_admin (i) {
+	
+	int n, op;
+	
+	printf("Bienvenido %s ", v_usuarios[i].Nomb_usuario);
+	printf("\nHas accedido como: ADMINISTRADOR");
+	
+	MENU:
+		
+	do {
+		
+		printf("\n\nMENU\n\n");
+	
+		printf("¿A que desea acceder?\n");
+	
+		printf("\t1.Usuarios\n");
+		printf("\t2.Alumnos\n");
+		printf("\t3.Materias\n");
+		printf("\t4.Horarios\n");
+		printf("\t5.Salir del programa\n");
+		
+		scanf("%d", &n);
+			
